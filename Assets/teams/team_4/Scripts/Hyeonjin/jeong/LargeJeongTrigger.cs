80a9c6593fe2b731a7c9012c3559b94924dbbdbe
h-zzn <hyeonjin@kaist.ac.kr>
2025-11-24 16:33:30 +0900

    large / small jeong 추가, 기능 구현

---
+++ Assets/teams/team_4/Scripts/Hyeonjin/jeong/LargeJeongTrigger.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class LargeJeongTrigger : MonoBehaviour
+{
+    private bool handWasInside = false;  // 손이 들어왔던 기록
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // 손이 아니면 무시
+        if (other.GetComponent<HandMarker>() == null)
+            return;
+
+        // 손이 새로 "들어온 순간"만 small Jeong 생성
+        if (!handWasInside)
+        {
+            SpawnSmallJeong();
+            handWasInside = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        // 손 Collider가 나가면 리셋
+        if (other.GetComponent<HandMarker>() != null)
+        {
+            handWasInside = false;
+        }
+    }
+
+    private void SpawnSmallJeong()
+    {
+        Debug.Log("Small Jeong Spawned (trigger event).");
+
+        // 작은 정 생성
+        FindObjectOfType<JeongController>()?.SpawnSmallJeong(transform.position);
+
+        // 큰 정 애니메이션 재생 (Trigger 파라미터)
+        animator.SetTrigger("isTouched");
+    }
+}
